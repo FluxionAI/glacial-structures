2a783f6252de374ba8f8b7a5ef4e536f7bdb6866
abhinand-arun <abhinand.a@hybridsolutions.com>
2025-11-20 15:14:09 +0530

    bubble sort

--- array/bubble_sort.go
+++ array/bubble_sort.go
@@ -3,14 +3,14 @@ package array
 func BubbleSortArray(array []int) []int {
 	n := len(array)
 	for i := 0; i < n-1; i++ {
-		swap := false
+		swapped := false
 		for j := 0; j < n-1-i; j++ {
 			if array[j] > array[j+1] {
 				array[j], array[j+1] = array[j+1], array[j]
-				swap = true
+				swapped = true
 			}
 		}
-		if swap {
+		if !swapped {
 			break
 		}
 	}
--- array/bubble_sort_test.go
+++ array/bubble_sort_test.go
@@ -9,7 +9,13 @@ func TestBubbleSortArray(t *testing.T) {
 	tests := []struct {
 		array, expected []int
 	}{
-		{[]int{3, 1, 5, 5, 6}, []int{1, 3, 5, 5, 6}},
+		{[]int{3, 1, 5, 5, 6}, []int{1, 3, 5, 5, 6}}, // basic
+		{[]int{}, []int{}},                           // empty array
+		{[]int{10}, []int{10}},                       // single element
+		{[]int{9, 8, 7, 6, 5}, []int{5, 6, 7, 8, 9}}, // reverse sorted
+		{[]int{4, 4, 4, 4}, []int{4, 4, 4, 4}},       // all duplicates
+		{[]int{-3, 10, -1, 7}, []int{-3, -1, 7, 10}}, // mix of negative & positive
+
 	}
 
 	for i, example := range tests {
